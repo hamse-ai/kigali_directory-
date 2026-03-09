13f07a783bbb4f0a9d72f9a2d48dd391d36cc1a3
hamse-ai <h.jama@alustudent.com>
2026-03-09 19:12:37 +0300

    chore: add bottom navigator

---
+++ lib/screens/main_navigation.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'directory_screen.dart';
+// import 'my_listings_screen.dart';
+// import 'map_full_view_screen.dart';
+// import 'settings_screen.dart';
+
+class MainNavigation extends StatefulWidget {
+  const MainNavigation({super.key});
+
+  @override
+  State<MainNavigation> createState() => _MainNavigationState();
+}
+
+class _MainNavigationState extends State<MainNavigation> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = [
+    const DirectoryScreen(),
+    // const MyListingsScreen(),
+    // const MapFullViewScreen(),
+    // const SettingsScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: (index) => setState(() => _selectedIndex = index),
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: const Color(0xFF0D1B3E),
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.bookmark), label: 'My Places'),
+          BottomNavigationBarItem(icon: Icon(Icons.map), label: 'Map'),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings'),
+        ],
+      ),
+    );
+  }
+}
